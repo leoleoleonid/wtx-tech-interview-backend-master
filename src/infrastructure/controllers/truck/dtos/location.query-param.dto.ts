32eecc84bb0737e1d8108f33d3ed7c6a0ca3c98f
leoleoleonid <leonidkoss1@gmail.com>
2023-03-19 04:24:25 +0200

    add validation

--- src/infrastructure/controllers/truck/dtos/location.query-param.dto.ts
+++ src/infrastructure/controllers/truck/dtos/location.query-param.dto.ts
@@ -1,10 +1,10 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsEnum } from 'class-validator';
 import {ApiProperty} from "@nestjs/swagger";
 import {LocationEnum} from "../../../../domain/model/truck";
 
 export class LocationDto {
-  @IsString()
+  @IsEnum(LocationEnum)
   @IsOptional()
-  @ApiProperty({required: false})
+  @ApiProperty({required: false,  enum: LocationEnum})
   location?: LocationEnum;
 }
